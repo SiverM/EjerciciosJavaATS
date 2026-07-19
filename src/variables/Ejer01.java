ff878e46f22e8590035e0dc9bf06a13ea2a0f834
Iver Medina <ivermedina55@gmail.com>
2026-07-18 23:29:26 -0400

    formato automatico java en intellij

--- src/variables/Ejer01.java
+++ src/variables/Ejer01.java
@@ -10,18 +10,19 @@
  * Luego imprima toda la informacion
  **/
 package variables;
+
 public class Ejer01 {
 
     public static void main(String[] args) {
-        String nombre="Iver";		// String no es un tipo de variable primitivo
-        int edad=40;
-        float estatura=1.70f;
-        boolean estudiante=false;
-        char inic_ap='M';
-        System.out.println("Mi nombre es: "+nombre);
-        System.out.println("Mi edad es de "+edad+" años");
-        System.out.println("Tengo una estatura de "+estatura+" mtrs.");
-        System.out.println("Soy estudiante?: "+estudiante);
-        System.out.println("La inician de mi apellido es: "+inic_ap);
+        String nombre = "Iver";        // String no es un tipo de variable primitivo
+        int edad = 40;
+        float estatura = 1.70f;
+        boolean estudiante = false;
+        char inic_ap = 'M';
+        System.out.println("Mi nombre es: " + nombre);
+        System.out.println("Mi edad es de " + edad + " años");
+        System.out.println("Tengo una estatura de " + estatura + " mtrs.");
+        System.out.println("Soy estudiante?: " + estudiante);
+        System.out.println("La inician de mi apellido es: " + inic_ap);
     }
 }
